8f3c28023fbdc88fa13524e0fc9127a08b499a7c
abby-flynn <84679083+abby-flynn@users.noreply.github.com>
2021-05-23 13:25:20 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".play").click(function() {
+    $("h1").hide();
+    $(".play").hide();
+    $(".title").hide();
+    $(".one").hide();
+    $(".stick one").hide();
+    $(".choice1").show();
+});
+$(".option1").click(function() {
+    $(".choice1").hide();
+    $(".cali").show();
+});
+$(".flycali").click(function() {
+    $(".cali").hide();
+    $(".flycalidie").show();
+});
+$(".drivecali").click(function() {
+    $(".cali").hide();
+    $(".wheretocali").show();
+});
+$(".option2").click(function() {
+    $(".choice1").hide();
+    $(".chicago").show();
+});
+$(".drivechicago").click(function() {
+    $(".chicago").hide();
+    $(".drivechicagodie").show();
+
+});
+$(".flychicago").click(function() {
+    $(".chicago").hide();
+    $(".wheretochicago").show();
+});
+$(".beachcali").click(function() {
+    $(".wheretocali").hide();
+    $(".beachcalidie").show();
+});
+$(".hotelchicago").click(function() {
+    $(".wheretochicago").hide();
+    $(".hotelchicagodie").show();
+});
+$(".hotelcali").click(function() {
+    $(".wheretocali").hide();
+    $(".caliending").show();
+});
+$(".foodchicago").click(function() {
+    $(".wheretochicago").hide();
+    $(".chicagoending").show();
+});
